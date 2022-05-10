2bc7a671b76c54b41e0882e5ea8d347ba364abef
Avery Kyle Arenas <kyruken@gmail.com>
2022-05-10 15:24:53 -0700

    add addNewLine function, fix set dimensions logic fully

--- javascript.js
+++ javascript.js
@@ -20,20 +20,40 @@ function deleteAllDivs() {
 
 }
 
+function addNewLine() {
+    const newLine = document.createElement('div');
+
+    newLine.classList.add('break');
+
+    return newLine;
+}
+
 function setDimensions() {
     let userInput = prompt("What dimensions would you like for the etch?");
     addDivToContainer(userInput);
 
 }
-function addDivToContainer(dimensions) {
+function addDivToContainer(number) {
 
+    let dimensions = number;
+    dimensions = dimensions * number;
     if (dimensions > 0) {
         //delete all the divs currently
         deleteAllDivs();
+
         //add new divs based on input
+        //newLineCounter dimensions-1 because we don't want a line break after first div
+        let newLineCounter = dimensions-1;
         for (let x = 0; x < dimensions; x++) {
             container.appendChild(createDiv());
             selectAllDivs();
+
+            //if newLineCounter is divisible by the number put in by user, we break into new row
+            //ex: user inputs 4 therefore a 4x4= 16, every 4n interval until less than 16 will have a break
+            if (newLineCounter % number === 0) {
+                container.appendChild(addNewLine());
+            }
+            newLineCounter--;
         }
         return;
      }
